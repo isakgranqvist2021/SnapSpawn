99f10a84abd3b94a2577e05892e1145ff66e7a89
Isak <contact@granqvist.dev>
2023-02-02 20:12:07 +0100

    Error handle function

--- functions/stripe.function.ts
+++ functions/stripe.function.ts
@@ -1,22 +1,38 @@
 import { Request, Response } from 'express';
-import { buffer } from 'micro';
 import { MongoClient } from 'mongodb';
 import Stripe from 'stripe';
-
-const client = new MongoClient(process.env.DATABASE_URL!);
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2022-11-15',
 });
 
+if (!stripe) {
+  throw new Error('Stripe is null');
+}
+
 async function addUserCredits(email: string, credits: number) {
   try {
-    await client.connect();
-    const db = client.db();
-    const collection = db.collection('users');
+    const c = new MongoClient(process.env.DATABASE_URL!);
+
+    if (!c) {
+      throw new Error('MongoClient is null');
+    }
+
+    const client = await c.connect();
+
+    if (!client) {
+      throw new Error('MongoClient is null');
+    }
+
+    const collection = client.db().collection('users');
+
+    if (!collection) {
+      throw new Error('Collection is null');
+    }
 
     return await collection.updateOne({ email }, { $inc: { credits } });
   } catch (err) {
+    console.error(err);
     return null;
   }
 }
@@ -50,42 +66,74 @@ function getWebhookEvent(
 
     return event;
   } catch (err) {
+    console.error(err);
     return null;
   }
 }
 
 export default async function (req: Request, res: Response) {
-  if (req.method !== 'POST') {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
-    return;
+  try {
+    if (req.method !== 'POST') {
+      res.setHeader('Allow', 'POST');
+      res.status(405).end('Method Not Allowed');
+      return;
+    }
+
+    const event = getWebhookEvent(req.body, req.headers['stripe-signature']);
+
+    if (!event) {
+      return res.status(400).send('Webhook Error: event is null');
+    }
+
+    if (event.type === 'payment_intent.succeeded') {
+      return res.status(202).send('Webhook received: PaymentIntent successful');
+    }
+
+    if (event.type === 'payment_intent.payment_failed') {
+      return res.status(400).send('Webhook received: PaymentIntent failed');
+    }
+
+    if (event.type === 'charge.succeeded') {
+      const paymentIntent = event.data.object as any;
+
+      if (!paymentIntent) {
+        return res
+          .status(400)
+          .send(
+            'Webhook received: Charge successful, but paymentIntent is null',
+          );
+      }
+
+      const credits = stripeAmountToCredits(paymentIntent.amount_captured);
+
+      if (!credits) {
+        return res
+          .status(400)
+          .send('Webhook received: Charge successful, but credits is null');
+      }
+
+      const email: string = paymentIntent.billing_details.email;
+
+      if (!email) {
+        return res
+          .status(400)
+          .send('Webhook received: Charge successful, but email is null');
+      }
+
+      const updateResult = await addUserCredits(email, credits);
+
+      if (updateResult?.modifiedCount === 1) {
+        return res.status(201).send('Webhook received: Charge successful');
+      }
+
+      return res
+        .status(400)
+        .send('Webhook received: Charge successful, but user not found');
+    }
+
+    return res.status(204).send('Webhook received: Unhandled event');
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('Webhook Error: Unhandled error');
   }
-
-  const buf = await buffer(req);
-  const event = getWebhookEvent(buf, req.headers['stripe-signature']);
-
-  if (!event) {
-    return res.status(400).send('Webhook Error: event is null');
-  }
-
-  if (event.type === 'payment_intent.succeeded') {
-    return res.status(202).send('Webhook received: PaymentIntent successful');
-  }
-
-  if (event.type === 'payment_intent.payment_failed') {
-    return res.status(400).send('Webhook received: PaymentIntent failed');
-  }
-
-  if (event.type === 'charge.succeeded') {
-    const paymentIntent = event.data.object as any;
-
-    const credits = stripeAmountToCredits(paymentIntent.amount_captured);
-    const email: string = paymentIntent.billing_details.email;
-
-    await addUserCredits(email, credits);
-
-    return res.status(201).send('Webhook received: Charge successful');
-  }
-
-  return res.status(204).send('Webhook received: Unhandled event');
 }
