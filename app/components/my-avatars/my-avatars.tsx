20d5fdfc4975a05a23527e74806b397230394f3b
Isak <contact@granqvist.dev>
2023-02-05 00:26:37 +0100

    Add little tile

--- app/components/my-avatars/my-avatars.tsx
+++ app/components/my-avatars/my-avatars.tsx
@@ -1,6 +1,7 @@
 import { useAppState } from '@aa/context';
 import { AvatarModel } from '@aa/models';
 import Image from 'next/image';
+import React from 'react';
 
 function renderAvatar(avatar: AvatarModel, index: number) {
   const { url } = avatar;
@@ -28,9 +29,17 @@ export function MyAvatars() {
   const appState = useAppState();
 
   return (
-    <div className="p-5 grid gap-5 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-      {appState.avatars.map(renderAvatar)}
-      {appState.avatars.length === 0 && <p>You have no avatars</p>}
-    </div>
+    <React.Fragment>
+      {appState.avatars.length > 0 && (
+        <h1 className="w-full px-5 pt-5 text-2xl font-bold">
+          My Avatars ({appState.avatars.length})
+        </h1>
+      )}
+
+      <div className="p-5 grid gap-5 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        {appState.avatars.map(renderAvatar)}
+        {appState.avatars.length === 0 && <p>You have no avatars</p>}
+      </div>
+    </React.Fragment>
   );
 }
