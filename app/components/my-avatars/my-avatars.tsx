e70be100ac19b0704a5f13c722f8dac294af6016
Isak <contact@granqvist.dev>
2023-02-04 15:50:39 +0100

    Der wi go

--- app/components/my-avatars/my-avatars.tsx
+++ app/components/my-avatars/my-avatars.tsx
@@ -2,23 +2,36 @@ import { useAppState } from '@aa/context';
 import { AvatarModel } from '@aa/models';
 import Image from 'next/image';
 
+function formatDateYYYYMMDDHHWithIntl(timestamp: number) {
+  return new Intl.DateTimeFormat(navigator.language, {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  }).format(timestamp);
+}
+
 function renderAvatar(avatar: AvatarModel, index: number) {
-  const { url } = avatar;
+  const { url, createdAt } = avatar;
 
   return (
-    <div className="card bg-base-100 shadow-xl" key={`avatar-${index}`}>
-      <a className="avatar" href={url}>
-        <div className="rounded">
-          <Image
-            width={256}
-            height={256}
-            src={url}
-            alt="Ai generated avatar"
-            loading="lazy"
-          />
-        </div>
-      </a>
-    </div>
+    <a
+      href={url}
+      key={`avatar-${index}`}
+      className="card bg-base-100 shadow-xl"
+    >
+      <figure>
+        <Image
+          width={256}
+          height={256}
+          src={url}
+          alt="Ai generated avatar"
+          loading="lazy"
+        />
+      </figure>
+      <div className="card-body p-0 md:p-3">
+        <p className="text-center">{formatDateYYYYMMDDHHWithIntl(createdAt)}</p>
+      </div>
+    </a>
   );
 }
 
@@ -26,7 +39,7 @@ export function MyAvatars() {
   const appState = useAppState();
 
   return (
-    <div className="py-5 flex flex-wrap gap-5 items-center justify-center">
+    <div className="py-5 grid gap-5 grid-cols-2 p-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
       {appState.avatars.map(renderAvatar)}
       {appState.avatars.length === 0 && <p>You have no avatars</p>}
     </div>
