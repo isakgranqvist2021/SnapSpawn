910491317c4e5bf48d36bf580c2a147fd7d65057
Isak <contact@granqvist.dev>
2023-02-04 20:55:42 +0100

    Fix weird issue with '

--- app/pages/error/index.tsx
+++ app/pages/error/index.tsx
@@ -22,11 +22,11 @@ export default function Error() {
       </Head>
       <MainContainer>
         <div className="flex flex-col gap-3 items-center mt-10">
-          <h1 className="text-xl text-green-600">Error </h1>
+          <h1 className="text-xl text-green-600">Error</h1>
           <p className="text-center max-w-prose">
-            Oops! The page you're looking for can't be found. It may have been
-            moved, renamed, or deleted. Please try checking the URL or using the
-            search function to find what you're looking for.
+            Oops! The page you are looking for could not be found. It may have
+            been moved, renamed, or deleted. Please try checking the URL or
+            using the search function to find what you are looking for.
           </p>
 
           <Link className="btn btn-error" href="/account">
