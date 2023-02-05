16a719e15e2c943db3bfa9efffd70cb74306fd80
Isak <contact@granqvist.dev>
2023-02-05 14:15:27 +0100

    test

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,7 +1,10 @@
+import 'dotenv/config';
+
 const env = {
   NODE_ENV: process.env.NODE_ENV as string,
 
   MONGO_DB_DATABASE_URL: process.env.MONGO_DB_DATABASE_URL as string,
+
   AUTH0_BASE_URL: process.env.AUTH0_BASE_URL as string,
   AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET as string,
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID as string,
