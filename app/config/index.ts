3c18170112fe0c7a55339e685f4f9b6acb65f377
Isak <contact@granqvist.dev>
2023-02-05 13:26:45 +0100

    Add all envs

--- app/config/index.ts
+++ app/config/index.ts
@@ -2,6 +2,11 @@ const env = {
   NODE_ENV: process.env.NODE_ENV as string,
 
   MONGO_DB_DATABASE_URL: process.env.MONGO_DB_DATABASE_URL as string,
+  AUTH0_BASE_URL: process.env.AUTH0_BASE_URL as string,
+  AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET as string,
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID as string,
+  AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL as string,
+  AUTH0_SECRET: process.env.AUTH0_SECRET as string,
 
   GCP_BUCKET_NAME: process.env.GCP_BUCKET_NAME as string,
   GCP_CLIENT_EMAIL: process.env.GCP_CLIENT_EMAIL as string,
