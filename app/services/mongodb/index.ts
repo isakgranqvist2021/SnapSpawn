dcf0f0d1ba96748b9338085d8734d1740a8a13b4
Isak <contact@granqvist.dev>
2023-02-05 11:59:13 +0100

    Some buh shit

--- app/services/mongodb/index.ts
+++ app/services/mongodb/index.ts
@@ -1,6 +1,8 @@
 import { env } from '@aa/config';
 import { MongoClient } from 'mongodb';
 
+console.log(process.env.NODE_ENV);
+
 declare namespace global {
   var _mongoClientPromise: Promise<MongoClient>;
 }
