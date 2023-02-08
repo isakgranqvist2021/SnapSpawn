f23ff45a221b886fc08db49f2aee268f3ac68b0d
Isak <contact@granqvist.dev>
2023-02-08 21:03:19 +0100

    Add logs

--- app/pages/api/auth/[...auth0].ts
+++ app/pages/api/auth/[...auth0].ts
@@ -1,11 +1,26 @@
 import { Logger } from '@aa/services/logger';
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleCallback } from '@auth0/nextjs-auth0';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 
 function onError(req: NextApiRequest, res: NextApiResponse, error: Error) {
   Logger.log('error', error.message, { req, res, error });
 }
 
+async function callback(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    await handleCallback(req, res, {
+      afterCallback: async (req, res, session) => {
+        Logger.log('info', 'afterCallback', { req, res, session });
+        return session;
+      },
+    });
+  } catch (err) {
+    Logger.log('error', err, { req, res, err });
+  }
+}
+
 export default handleAuth({
   onError,
+  callback,
 });
