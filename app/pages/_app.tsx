8a9427a10c8bfe2984165e699757dc3692fa1b08
Isak <contact@granqvist.dev>
2023-02-08 20:49:15 +0100

    log stuff

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { Logger } from '@aa/services/logger';
 import '@aa/styles/globals.css';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-import type { AppProps } from 'next/app';
+import type { AppProps, NextWebVitalsMetric } from 'next/app';
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  Logger.log('info', metric);
+}
 
 function App(props: AppProps) {
   const { Component, pageProps } = props;
--- app/pages/api/auth/[...auth0].ts
+++ app/pages/api/auth/[...auth0].ts
@@ -1,3 +1,11 @@
+import { Logger } from '@aa/services/logger';
 import { handleAuth } from '@auth0/nextjs-auth0';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default handleAuth();
+function onError(req: NextApiRequest, res: NextApiResponse, error: Error) {
+  Logger.log('error', error.message, { req, res, error });
+}
+
+export default handleAuth({
+  onError,
+});
