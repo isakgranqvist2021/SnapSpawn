a364222ea6c069cc9e36a356883e20a435748eaf
Isak <contact@granqvist.dev>
2023-02-08 08:08:51 +0100

    add some logs

--- app/pages/api/auth/[...auth0].ts
+++ app/pages/api/auth/[...auth0].ts
@@ -1,3 +1,11 @@
+import { Logger } from '@aa/services/logger';
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+export default handleAuth({
+  onError: (req, res, err) => {
+    Logger.log('info', req.url);
+    Logger.log('info', req.body);
+    Logger.log('info', req.headers);
+    Logger.log('error', err);
+  },
+});
