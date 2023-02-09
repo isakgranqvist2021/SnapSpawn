1de7160a247df9ce90dae071714512a70e576e01
Isak <contact@granqvist.dev>
2023-02-09 19:17:04 +0100

    qwtqwt

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -2,25 +2,21 @@ import { Head, Html, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
 const googleAnalytics = `
-<!-- Google tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-DMYWSZ00P0"></script>
-<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'G-DMYWSZ00P0');
-</script>
 `;
 
 export default function Document() {
   return (
     <Html lang="en" data-theme="light">
       <Head />
-      <Script id="google-analytics">{googleAnalytics}</Script>
       <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-DMYWSZ00P0"
       ></Script>
+      <Script id="google-analytics">{googleAnalytics}</Script>
       <body>
         <Main />
         <NextScript />
