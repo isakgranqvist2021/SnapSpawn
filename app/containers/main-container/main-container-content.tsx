87af74055f7837fc41b068b9ca859a721d891428
Isak Granqvist <isakwebdev@gmail.com>
2023-02-12 09:48:19 +0100

    Develop (#19)
    * Deploy new create transaction logic
    
    * Move workflow to a dir
    
    * Update workflow
    
    * test
    
    * wowqtqwt
    
    * qwtqwtqwt
    
    * qwtqwtqwt
    
    * Add footer
    
    * Remove cookie link from quick links
    
    * Fix build
    
    * Fix overflow bug
    
    ---------
    
    Co-authored-by: Isak <contact@granqvist.dev>

--- app/containers/main-container/main-container-content.tsx
+++ app/containers/main-container/main-container-content.tsx
@@ -4,7 +4,7 @@ export function MainContainerContent(props: PropsWithChildren) {
   const { children } = props;
 
   return (
-    <div className="h-full overflow-auto flex flex-col items-center flex-grow bg-base-100">
+    <div className="h-full flex flex-col items-center flex-grow bg-base-100">
       {children}
     </div>
   );
--- app/containers/main-container/main-container-layout.tsx
+++ app/containers/main-container/main-container-layout.tsx
@@ -3,5 +3,5 @@ import { PropsWithChildren } from 'react';
 export function MainContainerLayout(props: PropsWithChildren) {
   const { children } = props;
 
-  return <div className="flex h-full overflow-hidden bg-white">{children}</div>;
+  return <div className="flex h-full bg-white">{children}</div>;
 }
