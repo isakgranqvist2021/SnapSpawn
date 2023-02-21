38a1ce0e3945eb815ad0e0e21adb972fca1f3597
Isak <contact@granqvist.dev>
2023-02-21 20:07:01 +0100

    Add some pointless stuff

--- app/components/my-avatars/my-avatars.tsx
+++ app/components/my-avatars/my-avatars.tsx
@@ -1,8 +1,8 @@
-import { Spinner } from '@aa/components/spinner';
 import { useApiState } from '@aa/context/api-context';
 import { AvatarModel } from '@aa/models';
 import Image from 'next/image';
 import React from 'react';
+import { useState } from 'react';
 
 import { StatsCards } from '../stats-cards';
 
@@ -62,20 +62,115 @@ function renderAvatar(avatar: AvatarModel) {
   return <AvatarCard {...avatar} key={avatar.url} />;
 }
 
-export function MyAvatars() {
+function AvatarTableRow(props: AvatarModel) {
+  const { url, createdAt, prompt } = props;
+
+  const pills = prompt.split('&').map((pair) => {
+    const [_, value] = pair.split('=');
+
+    return value;
+  });
+
+  const renderPill = (pill: string, index: number) => {
+    return (
+      <div className="badge badge-md capitalize" key={`${url}-${index}`}>
+        {pill}
+      </div>
+    );
+  };
+
+  return (
+    <tr>
+      <td>
+        <div className="flex items-center space-x-3">
+          <div className="avatar">
+            <div className="mask mask-squircle w-12 h-12">
+              <Image width={32} height={32} src={url} alt="Avatar" />
+            </div>
+          </div>
+        </div>
+      </td>
+      <td>
+        <span className="text-gray-600">
+          {formatTimestampWithIntl(createdAt)}
+        </span>
+      </td>
+      <td>
+        <div className="flex gap-3">{pills.map(renderPill)}</div>
+      </td>
+      <td>
+        <a className="btn btn-ghost btn-xs" href={url}>
+          Download
+        </a>
+      </td>
+    </tr>
+  );
+}
+
+function AvatarTable() {
   const apiState = useApiState();
 
   const { avatars } = apiState;
 
   return (
+    <div className="overflow-x-auto w-full">
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th>Avatar</th>
+            <th>Date</th>
+            <th>Prompt</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {avatars.data.map((avatar) => (
+            <AvatarTableRow {...avatar} key={avatar.url} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export function MyAvatars() {
+  const apiState = useApiState();
+
+  const { avatars } = apiState;
+
+  const [mode, setMode] = useState('card');
+
+  const switchMode = () => {
+    setMode((mode) => (mode === 'card' ? 'list' : 'card'));
+  };
+
+  return (
     <React.Fragment>
-      <div className="pt-5 px-5 w-full">
-        <StatsCards />
+      <div className="pt-5 px-5 flex justify-between w-full lg:flex-nowrap flex-wrap gap-5">
+        <div className="w-full">
+          <StatsCards />
+        </div>
+
+        <label className="lg:flex hidden cursor-pointer label flex flex-row-reverse gap-5">
+          <span className="label-text whitespace-nowrap">
+            Toggle layout mode
+          </span>
+          <input
+            onChange={switchMode}
+            type="checkbox"
+            className="toggle toggle-primary"
+            checked={mode === 'card'}
+          />
+        </label>
       </div>
 
-      {avatars.data.length > 0 && (
+      {avatars.data.length > 0 && mode === 'card' ? (
         <div className="w-full p-5 my-avatars">
           {avatars.data.map(renderAvatar)}
+        </div>
+      ) : (
+        <div className="w-full p-5 ">
+          <AvatarTable />
         </div>
       )}
     </React.Fragment>
