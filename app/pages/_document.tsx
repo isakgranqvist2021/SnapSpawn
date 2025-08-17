09855356d1e9031a6a8181ad4502a8f47b42643a
isakgranqvist2021 <isakwebdev@gmail.com>
2025-08-17 13:45:56 +0200

    Pixel

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,8 +1,32 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en" data-theme="dark">
+      <Script id="fb-pixel" strategy="afterInteractive">
+        {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '742751983881717');
+            fbq('track', 'PageView');
+          `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=742751983881717&ev=PageView&noscript=1`}
+        />
+      </noscript>
+
       <Head />
 
       <body>
